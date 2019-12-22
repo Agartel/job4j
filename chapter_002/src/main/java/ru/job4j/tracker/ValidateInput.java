8cd2fc3015f1fe865b66c3b9a7108aca31576d9e
Agartel <gartel47@gmail.com>
2019-12-22 21:26:17 +0300

    last

--- chapter_002/src/main/java/ru/job4j/tracker/ValidateInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/ValidateInput.java
@@ -33,7 +33,7 @@ public class ValidateInput extends ConsoleInput {
             } catch (IllegalStateException moe) {
                 System.out.println("Please select key from menu ");
             } catch (NumberFormatException nfe) {
-                System.out.println("Please enter validate data again");///
+                System.out.println("Please enter validate data again");
             }
         } while (invalid);
         return value;
