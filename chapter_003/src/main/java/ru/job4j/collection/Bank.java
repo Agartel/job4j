f6e6cb22966fdb497ae81c37709381fe5d71ec24
Agartel <gartel47@gmail.com>
2020-01-02 16:10:34 +0300

    last

--- chapter_003/src/main/java/ru/job4j/collection/Bank.java
+++ chapter_003/src/main/java/ru/job4j/collection/Bank.java
@@ -1,9 +1,6 @@
 package ru.job4j.collection;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.Collectors;
 
@@ -19,29 +16,38 @@ public class Bank {
     }
     // добавить счёт пользователю.
     public void addAccountToUser(String passport, Account account) {
-        List<Account> accs = bank.entrySet().stream().filter(u -> u.getKey().getPassport().equals(passport)).map(u -> u.getValue());
-        //List<Account> accs = bank.get(new User(passport));
-        accs.add(account);
+        Optional<List<Account>> accs = bank.entrySet().stream().filter(u -> u.getKey().getPassport().equals(passport)).findFirst().map(u -> u.getValue());
+        accs.get().add(account);
     }
     // удалить один счёт пользователя.
     public void deleteAccountFromUser(String passport, Account account) {
-       // List<Account> accs = bank.entrySet().stream().filter(u -> u.getKey().equals(new User(passport))).map(a -> a.getValue()).flatMap(a -> a.stream()).collect(Collectors.toList());
-        List<Account> accs = bank.get(new User(passport));
-        accs.remove(accs.indexOf(account));
+        Optional<List<Account>> accs = bank.entrySet().stream().filter(u -> u.getKey().getPassport().equals(passport)).findFirst().map(u -> u.getValue());
+        accs.get().remove(accs.get().indexOf(account));
     }
     // получить список счетов для пользователя.
      public List<Account> getUserAccounts(String passport) {
-        /* List<Account> accs = new ArrayList<Account>();
-         for (Account acc : bank.get(new User(passport))) {
-             accs.add(acc);
-         }*/
-         List<Account> accs = bank.entrySet().stream().filter(u -> u.getKey().equals(new User(passport))).map(a -> a.getValue()).flatMap(a -> a.stream()).collect(Collectors.toList());
-
+         List<Account> accs = bank.entrySet().stream().filter(u -> u.getKey().getPassport().equals(passport)).map(a -> a.getValue()).flatMap(a -> a.stream()).collect(Collectors.toList());
         return accs;
      }
+
+    public Optional<Account> indexOf(String passport, String requisite) {
+        return bank.entrySet().stream().filter(u -> u.getKey().getPassport().equals(passport)).map(a -> a.getValue()).flatMap(l -> l.stream()).filter(srcAcc -> srcAcc.getRequisites().equals(requisite)).findFirst();
+    }
     // метод для перечисления денег с одного счёта на другой счёт:
     // если счёт не найден или не хватает денег на счёте srcAccount (с которого переводят) должен вернуть false.
     public boolean transferMoney(String srcPassport, String srcRequisite, String destPassport, String dstRequisite, double amount) {
+        boolean rsl = false;
+        Optional<Account> from = indexOf(srcPassport, srcRequisite);
+        Optional<Account> to = indexOf(destPassport, dstRequisite);
+        if (from.isPresent() && to.isPresent()) {
+            if (Double.compare(from.get().getValue(), amount) != -1) {
+                from.get().setValue(from.get().getValue() - amount);
+                to.get().setValue(to.get().getValue() + amount);
+                rsl = true;
+            }
+        }
+        return rsl;
+        /*
         AtomicBoolean result = new AtomicBoolean(false);
         bank.entrySet().stream().filter(u -> u.getKey().equals(new User(srcPassport))).map(a -> a.getValue()).flatMap(l -> l.stream()).filter(srcAcc -> srcAcc.getRequisites().equals(srcRequisite) && Double.compare(srcAcc.getValue(), amount) != -1).forEach(a -> {
             bank.entrySet().stream().filter(u -> u.getKey().equals(new User(destPassport))).map(b -> b.getValue()).flatMap(l -> l.stream()).filter(dstAcc -> dstAcc.getRequisites().equals(dstRequisite)).forEach(b -> {
@@ -50,7 +56,8 @@ public class Bank {
                 result.set(true);
             });
         });
-
+        return result.get();
+        */
         /* without streamAPI
         for (Account srcAcc : bank.get(new User(srcPassport))) {
             if (srcAcc.getRequisites().equals(srcRequisite)) {
@@ -69,6 +76,5 @@ public class Bank {
               }
             }
         } */
-        return result.get();
     }
 }
