fa6fa8f2345533a06680fb8d360a995930c814e0
Agartel <gartel47@gmail.com>
2020-01-23 00:27:05 +0300

    last

---
+++ chapter_001/src/main/java/ru/job4j/collectionspro/Converter.java
@@ -0,0 +1,46 @@
+package ru.job4j.collectionspro;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class Converter {
+    Iterator<Integer> convert(Iterator<Iterator<Integer>> it) {
+        return new Iterator<Integer>() {
+
+            Iterator<Integer> iter;
+
+            {
+                if (it.hasNext()) {
+                    iter = it.next();
+                } else {
+                    throw new NoSuchElementException("Не найден элемент");
+                }
+            }
+
+            @Override
+            public boolean hasNext() {
+                boolean res = false;
+                if (iter.hasNext()) {
+                        res = true;
+                }
+                return res;
+            }
+
+            @Override
+            public Integer next() {
+                int res = 0;
+                if (iter.hasNext()) {
+                    res = iter.next();
+                    if (!iter.hasNext() && it.hasNext()) {
+                        iter = it.next();
+                    }
+                } else {
+                    if (!it.hasNext()) {
+                        throw new NoSuchElementException("Не найден элемент");
+                    }
+                }
+                return res;
+            }
+        };
+    }
+}
