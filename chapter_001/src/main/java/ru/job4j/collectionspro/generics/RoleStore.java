9951fea402b1e78d17bce7233d5e12522120440c
Agartel <gartel47@gmail.com>
2020-01-25 13:27:05 +0300

    last

---
+++ chapter_001/src/main/java/ru/job4j/collectionspro/generics/RoleStore.java
@@ -0,0 +1,5 @@
+package ru.job4j.collectionspro.generics;
+
+public class RoleStore<T extends Role> extends AbstractStore<T> {
+
+}
---
+++ chapter_001/src/main/java/ru/job4j/collectionspro/generics/UserStore.java
@@ -0,0 +1,5 @@
+package ru.job4j.collectionspro.generics;
+
+public class UserStore<T extends User> extends AbstractStore<T>  {
+
+}
