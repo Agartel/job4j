4a39021834f9664b5b65dda15cbb617ba46ce545
Agartel <gartel47@gmail.com>
2020-01-26 17:44:06 +0300

    last

--- chapter_001/src/main/java/ru/job4j/collectionspro/iterator/EvenIterator.java
+++ chapter_001/src/main/java/ru/job4j/collectionspro/iterator/EvenIterator.java
@@ -13,41 +13,22 @@ public class EvenIterator implements Iterator {
         this.even = even;
     }
 
-    private final boolean isEven(int number) {
-        boolean res = false;
-        if (number % 2 == 0) {
-            res = true;
-        } else {
-            res = false;
-        }
-        return res;
-    }
-
-    private final boolean pointToEven() {
-        boolean res = false;
-        for (int i = idx; i < even.length; i++) {
-            if (isEven(even[i])) {
-                idx = i;
-                res = true;
-                break;
-            }
-        }
-        return res;
-    }
-
     @Override
     public boolean hasNext() {
-        return  pointToEven();
+        for (int i = idx; i < even.length; i++) {
+            if (even[i] % 2 == 0) {
+                idx = i;
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
     public Object next() {
-        int res = 0;
         if (!hasNext()) {
             throw new NoSuchElementException(errtxt);
         }
-        res = even[idx++];
-        hasNext();
-        return res;
+        return even[idx++];
     }
 }
