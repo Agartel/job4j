e5e0381c738c9c7e31614ef275f301212b510c9d
Agartel <gartel47@gmail.com>
2020-01-27 01:18:08 +0300

    last

--- chapter_001/src/main/java/ru/job4j/collectionspro/List/SimpleStack.java
+++ chapter_001/src/main/java/ru/job4j/collectionspro/List/SimpleStack.java
@@ -2,12 +2,22 @@ package ru.job4j.collectionspro.List;
 
 public class SimpleStack<T> {
     private DynamicLinkedArray<T> list = new DynamicLinkedArray<T>();
-    private int idx = 0;
+    private int idxpush = 0;
+    private int idxpoll = 0;
+    private T tmp;
+
     public T poll() {
-        return list.get(idx++);
+        if (idxpush > 0) {
+            tmp = list.get(idxpoll++);
+            idxpush--;
+        } else {
+            tmp = null;
+        }
+        return tmp;
     }
 
     public void push(T value) {
         list.add(value);
+        idxpush++;
     }
 }
--- chapter_001/src/test/java/ru/job4j/collectionspro/List/SimpleStackTest.java
+++ chapter_001/src/test/java/ru/job4j/collectionspro/List/SimpleStackTest.java
@@ -14,5 +14,22 @@ public class SimpleStackTest {
         stack.push(2);
         stack.push(3);
         assertThat(stack.poll(), is(3));
+        assertThat(stack.poll(), is(2));
+        assertThat(stack.poll(), is(1));
+        assertNull(stack.poll());
+    }
+
+    @Test
+    public void qwee() {
+        SimpleStack<Integer> stack = new SimpleStack<Integer>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        assertThat(stack.poll(), is(3));
+        assertThat(stack.poll(), is(2));
+        stack.push(44);
+        assertThat(stack.poll(), is(44));
+        assertThat(stack.poll(), is(1));
+        assertNull(stack.poll());
     }
 }
